81aa9f618b827cb67e4403cde55075a568fb8f75
OverTheCorner <gabrielalmario99@gmail.com>
2023-12-02 23:59:18 +0800

    test reading SPIFFS txt data

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,31 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <WebServer.h>
-#include <FS.h>
-
-// SSID & Password
-const char *ssid = "HomeAWiFi";                // Enter your SSID here
-const char *password = "gabriel99GlobeAtHome"; // Enter your Password here
-
-WebServer server(80); // Object of WebServer(HTTP port, 80 is defult)
-
-void handle_root();
+#include "SPIFFS.h"
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Try Connecting to ");
-  Serial.println(ssid);
 
-  // Connect to your wi-fi modem
-  WiFi.begin(ssid, password);
-
-  // Check wi-fi is connected to wi-fi network
-  while (WiFi.status() != WL_CONNECTED)
+  if (!SPIFFS.begin(true))
   {
-    delay(1000);
-    Serial.print(".");
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
   }
-  Serial.println("");
-  Serial.println("WiFi connected successfully");
-  Serial.print("Got IP: ");
-  Serial.println(WiFi.localIP()); // Show ESP32 IP on serial
 
-  server.on("/", handle_root);
+  File file = SPIFFS.open("/text.txt");
+  if (!file)
+  {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
 
-  server.begin();
-  Serial.println("HTTP server started");
-  delay(100);
+  Serial.println("File Content:");
+  while (file.available())
+  {
+    Serial.write(file.read());
+  }
+  file.close();
 }
 
 void loop()
 {
-  server.handleClient();
-}
-
-// Handle root url (/)
-void handle_root()
-{
-  // Open the HTML file
-  File file = SPIFFS.open("/index.html", "r");
-
-  if (file)
-  {
-    // Read the content of the file
-    String htmlContent = "";
-    while (file.available())
-    {
-      htmlContent += (char)file.read();
-    }
-
-    // Close the file
-    file.close();
-
-    // Send the HTML content as the response
-    server.send(200, "text/html", htmlContent);
-  }
-  else
-  {
-    server.send(500, "text/plain", "Internal Server Error");
-  }
 }
